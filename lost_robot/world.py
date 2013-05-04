0d7b4b83a0717746f7e0b79b544c38ecd4fd8b4d
pawel.rychly <pawelrychly@gmail.com>
2013-05-05 00:08:57 +0200

    Mało znaczące zmiany, dodana linijka kodu

--- lost_robot/world.py
+++ lost_robot/world.py
@@ -1,11 +1,14 @@
 __author__ = 'Paweł Rychły, Maciej Trojan'
 
+import robot.py
 
 class World:
     def __init__(self, width, height):
         self.width = width
         self.height = height
         self.landmarks = []
+        self.particles = []
+        self.robot = robot(self)
 
     def get_width(self):
         return self.width
@@ -15,3 +18,8 @@ class World:
 
     def get_landmarks(self):
         return self.landmarks
+
+    def add_landmark(self, landmark):
+        self.landmarks.append(landmark)
+
+
